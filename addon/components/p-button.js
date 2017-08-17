4a8015cfb932d77ed7d6fae36e443ac9002eb129
Alex DiLiberto <alex@alexdiliberto.com>
2017-08-17 19:57:14 -0400

    docs(yuidoc): add ember-cli-yuidoc

--- addon/components/p-button.js
+++ addon/components/p-button.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 import layout from '../templates/components/p-button';
 
+/**
+ * @module Components
+ */
+
+/**
+ * ```hbs
+ * {{#p-button color='red'}}Red Button{{/p-button}}
+ * ```
+ *
+ * @class p-button
+ */
+
 export default Ember.Component.extend({
   layout
 });
