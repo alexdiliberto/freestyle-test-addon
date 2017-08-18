7e3f95efaad621043118fdb5064be7b6881894c3
Alex DiLiberto <alex@alexdiliberto.com>
2017-08-17 20:00:23 -0400

    initial gh-pages setup

---
+++ addon/components/page-title.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import layout from '../templates/components/page-title';
+
+/**
+ * @module Components
+ */
+
+/**
+ * ```hbs
+ * {{#page-title}}Dummy Page Title{{/page-title}}
+ * ```
+ *
+ * @class page-title
+ */
+
+export default Ember.Component.extend({
+  layout
+});
---
+++ app/components/p-button.js
@@ -0,0 +1,1 @@
+export { default } from 'freestyle-test-addon/components/p-button';
